e34f152a5145282576ab284df59e3b00862595dd
Nijiko Yonskai <nijikokun@gmail.com>
2013-01-11 05:07:18 -0800

    Initial Start

---
+++ src/omg/icolite/iColite.java
@@ -0,0 +1,41 @@
+/**
+ * iColite
+ * 
+ * The lightweight, stable, and reliable economy replacement of iConomy. The future of Minecraft 
+ * Economy Ecosystems. Should I break all existing systems? Maybe.
+ *
+ * @copyright Copyright Nexua LLC (C) 2013
+ * @author Nijiko Yonskai (Nijikokun) <nijikokun@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+package omg.icolite;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class iColite extends JavaPlugin {
+  @Override public void onEnable () {
+    super.onEnable();
+  }
+
+  @Override public void onDisable () {
+    super.onDisable();
+  }
+
+  @Override public boolean onCommand (CommandSender sender, Command command, String label, String[] args) {
+    return super.onCommand(sender, command, label, args);
+  }
+}
